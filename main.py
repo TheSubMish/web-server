492d7e0bab233ac212b965ea65fb8207e2f35704
TheSubMish <subodhmishra015@gmail.com>
2025-07-20 09:22:25 +0545

    fix: app function

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from server.server import Server
 from server.reloader import start_with_reloader
+from app import app
 
 
 if __name__ == "__main__":
 
     def run_server():
         # Your server.run() here
-        Server("app").run()
+        Server(app).run()
 
     start_with_reloader(run_server)

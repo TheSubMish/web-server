5db234c5c82dae15b41557a5ae2122eb849ce619
TheSubMish <subodhmishra015@gmail.com>
2025-07-26 13:06:25 +0545

    fix: execute command more pythonic

--- managements/execute.py
+++ managements/execute.py
@@ -12,24 +12,35 @@ class ExecuteCommand:
     def __init__(self, args):
         self.args = args
 
-    def execute(self):
-        if len(self.args) > 1 and self.args[1] == "runserver":
-
-            try:
-                host = self.args[2] if len(self.args) > 2 else "127.0.0.1"
-                port = int(self.args[3]) if len(self.args) > 3 else 8000
-                if callable(run_server):
-                    run_server(host=host, port=port)
-                else:
-                    print("Error: run_server is not defined or not callable.")
-                    sys.exit(1)
-            except (IndexError, ValueError):
-                host = "127.0.0.1"
-                port = 8000
-
+    def _run_server(self):
+        try:
+            host = self.args[2] if len(self.args) > 2 else "127.0.0.1"
+            port = int(self.args[3]) if len(self.args) > 3 else 8000
             if callable(run_server):
                 run_server(host=host, port=port)
+            else:
+                print("Error: run_server is not defined or not callable.")
+                sys.exit(1)
+        except (IndexError, ValueError):
+            host = "127.0.0.1"
+            port = 8000
 
-        else:
-            print("Unknown command. Use 'runserver' to start the server.")
+        if callable(run_server):
+            run_server(host=host, port=port)
+
+    def execute(self):
+
+        if not self.args or len(self.args) < 2:
+            print("command not provided. Use 'runserver' to start the server.")
             sys.exit(1)
+
+        commands = {
+            "runserver": self._run_server,
+        }
+
+        if self.args[1] not in commands.keys():
+            print(f"Unknown command: {self.args[1]}")
+            sys.exit(1)
+
+        command = commands[self.args[1]]
+        command()
